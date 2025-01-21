873f2e4b2372c5bc237eabeb8e96e8b5afd645d7
Ola <rodzinkaaleksandra03@gmail.com>
2025-01-21 16:16:17 +0100

    dodalam 3 testy

--- Testy/UnitTest1.cs
+++ Testy/UnitTest1.cs
@@ -5,9 +5,35 @@ namespace Testy
     [TestClass]
     public class UnitTest1
     {
+        //sprawdzenie czy nazwa nowego laptopa dodala sie prawidlowo
         [TestMethod]
-        public void TestMethod1()
+        public void TestNazwaLaptopa()
         {
+
+            //arrange
+            string exppectedNAME = "Laptop testowy";
+            //action
+            Laptop laptop = new Laptop("Laptop testowy", 2000, "opis", "procesor", 32, 256, "karta graficzna");
+            //assert
+            Assert.AreEqual(exppectedNAME, laptop.Nazwa);
+        }
+        //sprawdzenie, czy stortowanie po cenie dziala poprawnie
+        [TestMethod]
+        public void TestCompareTo()
+        {
+            Tablet t1 = new Tablet("tablet test 1", 1000, "opis1", 23.6, "Android", true);
+            Tablet t2 = new Tablet("tablet test 2", 1087, "opis2", 20.6, "Android", true);
+            Assert.AreEqual(t1.CompareTo(t2), -1);
+
+        }
+        //sprawdzenie, czy metoda SprawdzDostepnosc wyrzuci odpowiedni wyjatek
+        [TestMethod]
+        [ExpectedException(typeof(ProduktNotFoundException))]
+        public void wyjatek()
+        {
+            Magazyn m1 = new();
+            m1.SprawdzDostepnosc(1);
+           
         }
     }
 }
