64a4e6eb974b4808e574627d41058145cad51f23
waclawik <kasiaw359@gmail.com>
2025-01-21 15:23:42 +0100

    Testy

---
+++ Testy/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Elektrosklep;
+
+namespace Testy
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
